0fa2eeb096790f97fa9eb6e2e8ac88177ac10818
arcmag <arcmag-worck@mail.ru>
2018-08-31 20:18:18 +0700

    Подгоняем разметку под макет

--- js/main.js
+++ js/main.js
@@ -2,10 +2,17 @@ var js = (function () {
     var js = {};
 
     js.toggleModal = function (modal, button, toggleClass) {
-        document.querySelector(button).addEventListener("click", function () {
-            document.querySelector(modal).classList.toggle(toggleClass);
-        });
+        var m = document.querySelector(modal);
+        if(m)
+            document.querySelector(button).addEventListener("click", function () {
+                m.classList.toggle(toggleClass);
+            });
     }
 
     return js;
 } ());
+
+
+
+
+js.toggleModal("#modal-window", "#hotel-search-btn", "hidden-modal");
