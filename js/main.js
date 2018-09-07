382f127214712a9b28fb7e86f05e502bcc0bec8d
arcmag <arcmag-worck@mail.ru>
2018-09-07 18:14:15 +0700

    Новые правки

--- js/main.js
+++ js/main.js
@@ -38,7 +38,6 @@ if(document.querySelector(".modal-animation-wrapper")) {
 
     js.toggleClass(".modal-animation-wrapper", "hidden-modal");
 
-    js.toggleClass("#alternative-map", "hidden");
     js.toggleClass(".map-search-hotels iframe", "hidden");
 
     document.querySelector("#hotel-search-btn").addEventListener("click", function (e) {
